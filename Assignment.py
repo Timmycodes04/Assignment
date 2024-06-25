95716366e80e38759883ef70682800e10fa44a24
Timmycodes04 <alani.timi@gmail.com>
2024-06-25 13:27:24 +0100

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -3,7 +3,14 @@ g=300
 t=150
 e=100
 m=50
-monthly                                         expenses=r+g+t+e+m
+monthly_expenses=r+g+t+e+m
 print('M')
 """project to determine the monthly budget of Mr and Mrs Bose. Where r is the rent, g is groceries
 t is the T-fare,e is for entertainment andd m for miscellenious"""
+
+
+C=224
+F=(C*9/5)+32
+print(F)
+
+""" Temperature app converter that is used to convert from degree celius(c) to degree fahrenheit(F)"""
