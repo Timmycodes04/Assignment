4c605874d7523a632b662d576c5c84d2d42f4022
Timmycodes04 <alani.timi@gmail.com>
2024-06-25 13:15:43 +0100

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -5,3 +5,5 @@ e=100
 m=50
 monthly                                         expenses=r+g+t+e+m
 print('M')
+"""project to determine the monthly budget of Mr and Mrs Bose. Where r is the rent, g is groceries
+t is the T-fare,e is for entertainment andd m for miscellenious"""
