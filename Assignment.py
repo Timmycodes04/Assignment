1feb1114f675685b3ab331445524df4531785a90
Timmycodes04 <alani.timi@gmail.com>
2024-06-25 13:57:15 +0100

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -14,3 +14,21 @@ F=(C*9/5)+32
 print(F)
 
 """ Temperature app converter that is used to convert from degree celius(c) to degree fahrenheit(F)"""
+
+current_year=2024
+birth_year=2012
+age=current_year-birth_year
+print(age)
+
+"""Age calculator program to calculate the current age of a person"""
+
+a=2
+b=5
+c=9
+d=1
+e=8
+mean=(a+b+c+d+e)/5
+print(mean)
+
+"""Write a program to find the mean of five numbers"""
+
